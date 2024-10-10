9b4f45e48b3421bdba260a9165627f6ee14b7669
Jakub Buler <buler.jakub@gmail.com>
2024-10-10 15:43:50 +0200

    compressed hypothesis 1 and added hypothesis 2

--- p2/main.py
+++ p2/main.py
@@ -1,6 +1,8 @@
 import sys
 import os
 sys.path.append(os.path.abspath('../')) 
+import pandas as pd
+import math
 import matplotlib.pyplot as plt
 import seaborn as sns
 import warnings
@@ -182,6 +184,10 @@ data_HDD['CZ_DOMINANT_PEAK_FORCE'] = data_HDD.apply(assign_dominant_extremity, l
 data_HDD['DW_DOMINANT_PEAK_FORCE'] = data_HDD.apply(assign_dominant_extremity, left_col='DW_L_PEAK_FORCE', right_col='DW_R_PEAK_FORCE', axis=1)
 data_HDD['BR_DOMINANT_PEAK_FORCE'] = data_HDD.apply(assign_dominant_extremity, left_col='BR_L_PEAK_FORCE', right_col='BR_R_PEAK_FORCE', axis=1)
 
+data_HDD['PS_NONDOMINANT_PEAK_FORCE'] = data_HDD.apply(assign_dominant_extremity, right_col='PS_L_PEAK_FORCE', left_col='PS_R_PEAK_FORCE', axis=1)
+data_HDD['CZ_NONDOMINANT_PEAK_FORCE'] = data_HDD.apply(assign_dominant_extremity, right_col='CZ_L_PEAK_FORCE', left_col='CZ_R_PEAK_FORCE', axis=1)
+data_HDD['DW_NONDOMINANT_PEAK_FORCE'] = data_HDD.apply(assign_dominant_extremity, right_col='DW_L_PEAK_FORCE', left_col='DW_R_PEAK_FORCE', axis=1)
+data_HDD['BR_NONDOMINANT_PEAK_FORCE'] = data_HDD.apply(assign_dominant_extremity, right_col='BR_L_PEAK_FORCE', left_col='BR_R_PEAK_FORCE', axis=1)
 
 
 data_YBT['YBT_ANT_DOMINANT'] = data_YBT.apply(assign_dominant_extremity, left_col='YBT_ANT_L_Normalized', right_col='YBT_ANT_R_Normalized', axis=1)
@@ -195,76 +201,214 @@ data_YBT['YBT_PL_NONDOMINANT'] = data_YBT.apply(assign_dominant_extremity, right
 data_YBT['YBT_COMPOSITE_NONDOMINANT'] = data_YBT.apply(assign_dominant_extremity, right_col='YBT_COMPOSITE_L', left_col='YBT_COMPOSITE_R', axis=1)
 
 
-
 # %%
 # Hypothesis 1
 # H1 - Zawodnicy z wysoką specjalizacją (High) będą wykazywać
 #  niższe wyniki w Sports Performance Tests
-sns.set_theme(style="ticks", palette="pastel")
 
-fig, axes = plt.subplots(4, 1, figsize=(10, 10))
+def create_boxplot_4row(data, id_var, value_vars_list, row_labels, hue_label, legend_labels, palette, order, x_label):
+    sns.set_theme(style="ticks", palette="pastel")
 
-df_melted = data_YBT.melt(id_vars='Sports_Specialization',
-                          value_vars=['YBT_ANT_DOMINANT', 'YBT_ANT_NONDOMINANT'],
-                          var_name='Dominant Extremity', value_name='Score')
+    fig, axes = plt.subplots(4, 1, figsize=(10, 10))
 
-sns.boxplot(ax=axes[0], x='Sports_Specialization', y='Score',
-            hue='Dominant Extremity',
-            data=df_melted, showfliers=False,
-            palette=["m", "g"], order=['low', 'moderate', 'high'])
-axes[0].yaxis.set_label_text('YBT ANT')
+    for i, value_vars in enumerate(value_vars_list):
+        df_melted = data.melt(id_vars=id_var,
+                              value_vars=value_vars,
+                              var_name=hue_label, value_name='Score')
 
-df_melted = data_YBT.melt(id_vars='Sports_Specialization',
-                          value_vars=['YBT_PM_DOMINANT', 'YBT_PM_NONDOMINANT'],
-                          var_name='Dominant Extremity', value_name='Score')
+        sns.boxplot(ax=axes[i], x=id_var, y='Score',
+                    hue=hue_label,
+                    data=df_melted, showfliers=False,
+                    palette=palette, order=order)
+        axes[i].yaxis.set_label_text(row_labels[i])
 
-sns.boxplot(ax=axes[1], x='Sports_Specialization', y='Score',
-            hue='Dominant Extremity',
-            data=df_melted, showfliers=False,
-            palette=["m", "g"], order=['low', 'moderate', 'high'])
-axes[1].yaxis.set_label_text('YBT PM')
+    for ax in axes:
+        ax.get_legend().remove()
+        ax.get_xaxis().get_label().set_visible(False)
+        ax.grid(axis='y')
 
-df_melted = data_YBT.melt(id_vars='Sports_Specialization',
-                          value_vars=['YBT_PL_DOMINANT', 'YBT_PL_NONDOMINANT'],
-                          var_name='Dominant Extremity', value_name='Score')
+    handles, _ = axes[0].get_legend_handles_labels()  # Get handles and labels from one of the plots
+    fig.legend(handles, legend_labels, loc='lower center', title=hue_label,
+               ncol=3, bbox_to_anchor=(0.5, 1), frameon=False)
 
-sns.boxplot(ax=axes[2], x='Sports_Specialization', y='Score',
-            hue='Dominant Extremity',
-            data=df_melted, showfliers=False,
-            palette=["m", "g"], order=['low', 'moderate', 'high'])
-axes[2].yaxis.set_label_text('YBT PL')
+    plt.tight_layout(rect=[0, 0.03, 1, 1])
+    [ax.set_xticklabels([]) for ax in axes[:-1]]
 
-df_melted = data_YBT.melt(id_vars='Sports_Specialization',
-                          value_vars=['YBT_COMPOSITE_DOMINANT', 'YBT_COMPOSITE_NONDOMINANT'],
-                          var_name='Dominant Extremity', value_name='Score')
+    axes[3].get_xaxis().get_label().set_visible(True)
+    axes[3].set_xlabel(x_label)
+    plt.tight_layout()
+    plt.show()
 
-sns.boxplot(ax=axes[3], x='Sports_Specialization', y='Score',
-            hue='Dominant Extremity',
-            data=df_melted, showfliers=False,
-            palette=["m", "g"], order=['low', 'moderate', 'high'])
-axes[3].yaxis.set_label_text('YBT COMP')
-
-for ax in axes:
-    ax.get_legend().remove()
-    ax.get_xaxis().get_label().set_visible(False)
-
-# Add a single legend at the bottom of all subplots
-handles, _ = axes[0].get_legend_handles_labels()  # Get the handles and labels from one of the plots
-labels = ['Dominant', 'Nondominant']
-fig.legend(handles, labels, loc='lower center', title= 'Extremity',
-           ncol=3, bbox_to_anchor=(0.5, 1), frameon=False)
-
-# Adjust the layout to make room for the legend
-plt.tight_layout(rect=[0, 0.03, 1, 1])  # Adjust the bottom space to fit the legend
-[ax.set_xticklabels([]) for ax in axes[:-1]]
-
-axes[3].get_xaxis().get_label().set_visible(True)
-axes[3].set_xlabel('Sports Specialization')
-plt.tight_layout()
-plt.show()
-
+create_boxplot_4row(
+    data=data_YBT, 
+    id_var='Sports_Specialization', 
+    value_vars_list=[
+        ['YBT_ANT_DOMINANT', 'YBT_ANT_NONDOMINANT'],
+        ['YBT_PM_DOMINANT', 'YBT_PM_NONDOMINANT'],
+        ['YBT_PL_DOMINANT', 'YBT_PL_NONDOMINANT'],
+        ['YBT_COMPOSITE_DOMINANT', 'YBT_COMPOSITE_NONDOMINANT']
+    ], 
+    row_labels=['YBT ANT', 'YBT PM', 'YBT PL', 'YBT COMP'], 
+    hue_label='Dominant Extremity', 
+    legend_labels=['Dominant', 'Nondominant'], 
+    palette=["m", "g"], 
+    order=['low', 'moderate', 'high'], 
+    x_label='Sports Specialization'
+)
+create_boxplot_4row(
+    data=data_HDD, 
+    id_var='Sports_Specialization', 
+    value_vars_list=[
+        ['PS_DOMINANT_PEAK_FORCE', 'PS_NONDOMINANT_PEAK_FORCE'],
+        ['CZ_DOMINANT_PEAK_FORCE', 'CZ_NONDOMINANT_PEAK_FORCE'],
+        ['DW_DOMINANT_PEAK_FORCE', 'DW_NONDOMINANT_PEAK_FORCE'],
+        ['BR_DOMINANT_PEAK_FORCE', 'BR_NONDOMINANT_PEAK_FORCE']
+    ], 
+    row_labels=['HDD PS', 'HDD CZ', 'HDD DW', 'HDD BR'], 
+    hue_label='Dominant Extremity', 
+    legend_labels=['Dominant', 'Nondominant'], 
+    palette=["m", "g"], 
+    order=['low', 'moderate', 'high'], 
+    x_label='Sports Specialization'
+)
 
 # %%
 
+sns.boxplot(x='Sports_Specialization', y='FMS_TOTAL', data=data_FMS,
+            order=['low', 'moderate', 'high'])
+sns.stripplot(x='Sports_Specialization', y='FMS_TOTAL', data=data_FMS, color=".3")
+plt.xlabel('Sports Specialization')
+plt.ylabel('FMS Total Score')
+plt.grid(axis='y')
+plt.show()
+
+
+
+# %%
+# Hypothesis 2
+# H2 - Zawodnicy trenujący mniej główny sport („Training_Volume_Weekly_MainSport (hrs)”)
+# i wszystkie sporty „Training_Volume_Weekly_ALLsports(hrs)” będą wykazywać niższe wyniki
+# w Sports Performance Tests dla dominującej i niedominującej kończyny dolnej (YBT, HHD, FMS)
+
+
+def ybt_box_plot(data, x, y1, y2, title1, title2, xlabel, ylabel):
+    data_YBT = data
+# Cut dataframe into 4 equal bins based on x and name the ranges with numeric stats
+    bins = pd.cut(data_YBT[x], bins=4, include_lowest=True)
+    bin_labels = [f"{round(b.left, 1)}-{round(b.right, 1)}" for b in bins.cat.categories]
+    bin_labels[0] = str(min(data_YBT[x])) + bin_labels[0][3:]
+    #\2013 for n-dash (number ranges), \u2014 for m-dash
+    bin_labels = [f"{b}".replace('-', '\u2013') for b in bin_labels]
+
+    data_YBT['Training_Volume_Binned'] = pd.cut(data_YBT[x], bins=4,
+                                            labels=bin_labels, include_lowest=True)
+
+    fig, axes = plt.subplots(1, 2, figsize=(12, 6))
+
+    sns.boxplot(x='Training_Volume_Binned', y=y1, data=data_YBT, ax=axes[0])
+    sns.stripplot(x='Training_Volume_Binned', y=y1, data=data_YBT, ax=axes[0], color=".3")
+    axes[0].set_title(title1)
+    axes[0].set_ylabel(ylabel)
+
+    sns.boxplot(x='Training_Volume_Binned', y=y2, data=data_YBT, ax=axes[1])
+    sns.stripplot(x='Training_Volume_Binned', y=y2, data=data_YBT, ax=axes[1], color=".3")
+    axes[1].set_title(title2)
+    axes[1].set_ylabel(ylabel)
+    ylim_ax0 = axes[0].get_ylim()
+    ylim_ax1 = axes[1].get_ylim()
+    upper_lim = math.ceil(max(ylim_ax0[1], ylim_ax1[1]))
+    lower_lim = math.floor(min(ylim_ax0[0], ylim_ax1[0]))
+
+
+    # Remove individual x-axis labels
+    axes[0].set_xlabel('')
+    axes[1].set_xlabel('')
+    
+    # Set a shared x-axis label for the whole row
+    fig.supxlabel(xlabel)
+
+    for ax in axes:
+        ax.set_ylim([lower_lim, upper_lim])
+        ax.grid(axis='y')
+
+    plt.tight_layout()
+    plt.show()
+
+title1 = 'Dominant Extremity'
+title2 = 'Non Dominant Extremity'
+
+x = 'Training_Volume_Weekly_ALLSports'
+xlabel = 'Training Volume All Sports [hrs/week]'
+
+# x = 'Training_Volume_Weekly_MainSport'
+# xlabel = 'Training Volume Main Sport [hrs/week]'
+
+ybt_box_plot(data_YBT, x=x,
+             y1='YBT_ANT_DOMINANT',
+             y2='YBT_ANT_NONDOMINANT',
+             title1=title1,
+             title2=title2,
+             xlabel=xlabel,
+             ylabel='YBT ANT')
+ybt_box_plot(data_YBT, x=x,
+             y1='YBT_PM_DOMINANT',
+             y2='YBT_PM_NONDOMINANT',
+             title1=title1,
+             title2=title2,
+             xlabel=xlabel,
+             ylabel='YBT PM')
+ybt_box_plot(data_YBT, x=x,
+             y1='YBT_PL_DOMINANT',
+             y2='YBT_PL_NONDOMINANT',
+             title1=title1,
+             title2=title2,
+             xlabel=xlabel,
+             ylabel='YBT PL')
+ybt_box_plot(data_YBT, x=x,
+             y1='YBT_COMPOSITE_DOMINANT',
+             y2='YBT_COMPOSITE_NONDOMINANT',
+             title1=title1,
+             title2=title2, 
+             xlabel=xlabel,
+             ylabel='YBT COMP')
+
+# %%
+#FMS
+def fms_box_plot(data, x, y, title, xlabel, ylabel):
+    data_FMS = data
+    bins = pd.cut(data_FMS[x], bins=4, include_lowest=True)
+    bin_labels = [f"{round(b.left, 2)}-{round(b.right, 2)}" for b in bins.cat.categories]
+    bin_labels[0] = f"{min(data_FMS[x]):.2f}" + bin_labels[0][4:]
+    bin_labels = [f"{b}".replace('-', '\u2013') for b in bin_labels]
+    for b in bins.cat.categories:
+        print(b)
+    print(bin_labels)
+
+    data_FMS['Training_Volume_Binned'] = pd.cut(data_FMS[x], bins=4,
+                                            labels=bin_labels, include_lowest=True)
+
+    fig, ax = plt.subplots(figsize=(8, 6))
+
+    sns.boxplot(x='Training_Volume_Binned', y=y, data=data_FMS)
+    sns.stripplot(x='Training_Volume_Binned', y=y, data=data_FMS, color=".3")
+    ax.set_title(title)
+    ax.set_ylabel(ylabel)
+    ax.grid(axis='y')
+    ax.set_xlabel(xlabel)
+
+    plt.tight_layout()
+    plt.show()
+
+title = 'FMS Total Score'
+# x = 'Training_Volume_Weekly_MainSport'
+# xlabel = 'Training Volume Main Sport [hrs/week]'
+
+x = 'Training_Volume_Weekly_ALLSports'
+xlabel = 'Training Volume All Sports [hrs/week]'
+
+ylabel = 'FMS Total Score'
+
+fms_box_plot(data_FMS, x=x, y='FMS_TOTAL', title=title, xlabel=xlabel, ylabel=ylabel)
+#%%
 
 # %%
